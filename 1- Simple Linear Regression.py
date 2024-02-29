6c4b856804961f062f087638563f2d56a35cdd52
Omar Abdullah <121067316+omr-ql@users.noreply.github.com>
2024-02-29 17:23:39 +0200

    Add files via upload

---
+++ 1- Simple Linear Regression.py
@@ -0,0 +1,31 @@
+# Written By : Omar Abdullah Saeed 
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('Salary.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+y_pred = regressor.predict(X_test)
+
+if __name__ == "__main__" : 
+    # The Training Set : 
+    plt.scatter(X_train, y_train, color = 'red')
+    plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+    plt.title('Salary vs Experience (Training set)')
+    plt.xlabel('Years of Experience')
+    plt.ylabel('Salary')
+    plt.show()
+    # The Testing Set  :
+    plt.scatter(X_test, y_test, color = 'red')
+    plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+    plt.title('Salary vs Experience (Test set)')
+    plt.xlabel('Years of Experience')
+    plt.ylabel('Salary')
+    plt.show()
